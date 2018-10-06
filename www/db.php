c6304781dfd074fb6adc23bccd886d058a9bc33d
BachtiarYusuf <43898716+BachtiarYusuf@users.noreply.github.com>
2018-10-06 12:01:21 +0700

    Update db.php

--- www/db.php
+++ www/db.php
@@ -1,4 +1,4 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("localhost","root","root","phonegap_demo") or die ("could not connect database");
+ $con = mysqli_connect("localhost","id7374461_bachtiary","bachtiar098","id7374461_bachtiar") or die ("could not connect database");
 ?>
